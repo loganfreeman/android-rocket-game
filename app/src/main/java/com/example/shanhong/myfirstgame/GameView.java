3e249739a03a239859b56a8b45f2ddcc3e6452e6
shanhong cheng <shanhong.cheng@utah.edu>
2016-12-01 16:01:14 -0700

    initial commit

---
+++ app/src/main/java/com/example/shanhong/myfirstgame/GameView.java
@@ -0,0 +1,211 @@
+package com.example.shanhong.myfirstgame;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.util.ArrayList;
+
+import static com.example.shanhong.myfirstgame.R.drawable.boom;
+
+/**
+ * Created by shanhong on 12/1/16.
+ */
+
+public class GameView extends SurfaceView implements Runnable {
+
+    //boolean variable to track if the game is playing or not
+    volatile boolean playing;
+
+    //the game thread
+    private Thread gameThread = null;
+
+    //adding the player to this class
+    private Player player;
+
+    //These objects will be used for drawing
+    private Paint paint;
+    private Canvas canvas;
+    private SurfaceHolder surfaceHolder;
+
+    //Adding an stars list
+    private ArrayList<Star> stars = new
+            ArrayList<Star>();
+
+    //Adding enemies object array
+    private Enemy[] enemies;
+
+    //Adding 3 enemies you may increase the size
+    private int enemyCount = 3;
+
+    //defining a boom object to display blast
+    private Boom boom;
+
+
+    //Class constructor
+    public GameView(Context context, int screenX, int screenY) {
+        super(context);
+
+        //initializing player object
+        player = new Player(context, screenX, screenY);
+
+        //initializing drawing objects
+        surfaceHolder = getHolder();
+        paint = new Paint();
+
+        //adding 100 stars you may increase the number
+        int starNums = 100;
+        for (int i = 0; i < starNums; i++) {
+            Star s  = new Star(screenX, screenY);
+            stars.add(s);
+        }
+
+        //initializing enemy object array
+        enemies = new Enemy[enemyCount];
+        for(int i=0; i<enemyCount; i++){
+            enemies[i] = new Enemy(context, screenX, screenY);
+        }
+
+        //initializing boom object
+        boom = new Boom(context);
+
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_UP:
+                //When the user presses on the screen
+                //we will do something here
+                player.stopBoosting();
+                break;
+            case MotionEvent.ACTION_DOWN:
+                //When the user releases the screen
+                //do something here
+                player.setBoosting();
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void run() {
+        while (playing) {
+            //to update the frame
+            update();
+
+            //to draw the frame
+            draw();
+
+            //to control
+            control();
+        }
+    }
+
+
+    private void update() {
+//updating player position
+        player.update();
+
+        //setting boom outside the screen
+        boom.setX(-250);
+        boom.setY(-250);
+
+        //Updating the stars with player speed
+        for (Star s : stars) {
+            s.update(player.getSpeed());
+        }
+
+        //updating the enemy coordinate with respect to player speed
+        for(int i=0; i<enemyCount; i++){
+            enemies[i].update(player.getSpeed());
+
+            //if collision occurrs with player
+            if (Rect.intersects(player.getDetectCollision(), enemies[i].getDetectCollision())) {
+
+                //displaying boom at that location
+                boom.setX(enemies[i].getX());
+                boom.setY(enemies[i].getY());
+                //moving enemy outside the left edge
+                enemies[i].setX(-200);
+            }
+        }
+    }
+
+    private void draw() {
+//checking if surface is valid
+        if (surfaceHolder.getSurface().isValid()) {
+            //locking the canvas
+            canvas = surfaceHolder.lockCanvas();
+            //drawing a background color for canvas
+            canvas.drawColor(Color.BLACK);
+
+            //setting the paint color to white to draw the stars
+            paint.setColor(Color.WHITE);
+
+            //drawing all stars
+            for (Star s : stars) {
+                paint.setStrokeWidth(s.getStarWidth());
+                canvas.drawPoint(s.getX(), s.getY(), paint);
+            }
+            //Drawing the player
+            canvas.drawBitmap(
+                    player.getBitmap(),
+                    player.getX(),
+                    player.getY(),
+                    paint);
+
+            //drawing the enemies
+            for (int i = 0; i < enemyCount; i++) {
+                canvas.drawBitmap(
+                        enemies[i].getBitmap(),
+                        enemies[i].getX(),
+                        enemies[i].getY(),
+                        paint
+                );
+            }
+
+            //drawing boom image
+            canvas.drawBitmap(
+                    boom.getBitmap(),
+                    boom.getX(),
+                    boom.getY(),
+                    paint
+            );
+            //Unlocking the canvas
+            surfaceHolder.unlockCanvasAndPost(canvas);
+        }
+    }
+
+    private void control() {
+        try {
+            gameThread.sleep(17);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pause() {
+        //when the game is paused
+        //setting the variable to false
+        playing = false;
+        try {
+            //stopping the thread
+            gameThread.join();
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public void resume() {
+        //when the game is resumed
+        //starting the thread again
+        playing = true;
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+}
---
+++ app/src/main/java/com/example/shanhong/myfirstgame/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.shanhong.myfirstgame;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageButton buttonPlay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //setting the orientation to landscape
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+        //getting the button
+        buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
+
+        //adding a click listener
+        buttonPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        startActivity(new Intent(this, GameActivity.class));
+    }
+}
